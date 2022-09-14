7cc6a08716f70eef645608050c1c5843cf698d01
Carles Roch-Cunill <carles.roch-cunill@gov.bc.ca>
2022-09-14 11:28:51 -0700

    Latest scripts

--- convertToHTML.py
+++ convertToHTML.py
@@ -32,7 +32,13 @@ def convertKeyParam(resultsFile,LZ):
     html=html+ "<hr class=\"dashed\">\n"
 
     html=html+ "<H2>LZ" + LZ + "</B> :  configuration values</H2>\n"
-    html=html+"<P><B>Number of AWS IAM users in LZ" + LZ + "</B> : " + str(jsonData["awsNumberIamUsers"])+ " </P>\n"
+    
+    
+    
+    
+    
+    
+    
     html=html+"<P><B>Number of AWS IAM groups in LZ" + LZ + "</B> : " + str(jsonData["awsNumberIamGroups"]) + " </P>\n"
     html=html+"<P><B>Number of AWS IAM policies in LZ" + LZ + "</B> : " +str(jsonData["awsNumberIamPolicies"]) + " </P>\n"
 
@@ -42,6 +48,17 @@ def convertKeyParam(resultsFile,LZ):
     html=html+"<P><B>Number of Cloudfront Functions associated to this account</B> : " +str(jsonData["numberCloudfrontFunctions"]) + " </P>\n"
     html=html+"<P><B>Number of clusters associated to this account</B> : " +str(jsonData["numberClusters"]) + " </P>\n"
     html=html+"<P><B>Number of EC2 instances associated to this account</B> : " +str(jsonData["numberEC2Instances"]) + " </P>\n"    
+   
+    html=html+ "<hr class=\"dashed\">\n"
+    html=html+ "<H2>AWS IAM users</H2>\n"
+    html=html+"<P><B>Number of AWS IAM users in LZ" + LZ + "</B> : " + str(jsonData["awsNumberIamUsers"])+ " </P>\n"
+    html=html+ "<H3>List of AWS IAM users - <I>Name : Number of attached policies</I></H3>\n"
+    if str(jsonData["awsNumberIamUsers"])!='n/a' and int(jsonData["awsNumberIamUsers"])>0:
+        html=html+"<UL>"
+        jsn_list = jsonData['ListIAMUsers']
+        for key in jsn_list:
+            html=html+"<LI><B>      " + key + "</B> : " + str(jsn_list[key]) + " </LI>\n"
+        html=html+"</UL>"
    
     html=html+ "<hr class=\"dashed\">\n"
     html=html+ "<H2>Accounts</H2>\n"
@@ -136,7 +153,7 @@ def convertPoliciesRoles(resultsFile,LZ):
 
     html=html+ "<hr class=\"dashed\">\n"
 
-    html=html+ "<H2>Managed Policies Attached to IAM Role</H2>\n"
+    html=html+ "<H2>Managed Policies Attached to IAM Role (<I>Name : number</I>)</H2>\n"
     
     #jsn_list = jsonData['S3Buckets_AccessPolicy']
     html=html+"<UL>"
@@ -147,7 +164,7 @@ def convertPoliciesRoles(resultsFile,LZ):
     html=html+"</UL>"
     
     html=html+ "<hr class=\"dashed\">\n"
-    html=html+ "<H2>In Line Policies Embedded to IAM Role</H2>\n"
+    html=html+ "<H2>In Line Policies Embedded to IAM Role  (<I>Name : number</I>)</H2>\n"
     html=html+"<UL>"
     for key in jsonData:
         mykey=key.split("_",1)
